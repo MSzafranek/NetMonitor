cebbd4ef2ab18ca99e2a3c69722552da60319b99
KRunchPL <krunchfrompoland@gmail.com>
2021-01-29 14:12:18 +0100

    Initial commit

---
+++ src/my_project/dummy.py
@@ -0,0 +1,12 @@
+"""
+Dummy module.
+"""
+
+
+def return_true() -> bool:
+    """
+    Tell the truth.
+
+    :return: you can't handle the truth!
+    """
+    return True
---
+++ tests/my_project/test_dummy.py
@@ -0,0 +1,5 @@
+from my_project.dummy import return_true
+
+
+def test_dummy():
+    assert return_true()
